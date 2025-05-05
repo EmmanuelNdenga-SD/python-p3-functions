fcd2053bdae58ac7f661a398601b038e91720cd0
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-05-05 16:54:55 +0300

    add functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,21 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
-def greet(name):
+def greet(name="Emmanuel"):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
-
-def add(num1, num2):
+    print(f"Hello, {name}!")
+    
+def add(num1=1, num2=2):
     pass
+    return num1 + num2
 
-def halve(number):
-    pass
+def halve(number=10):
+    pass    
+    return number / 2
+
